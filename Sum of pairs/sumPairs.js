77c64a0820b9cf13f7b7953b406136d4271161ea
ifkerri <ifkerri@mail.ru>
2022-04-06 15:34:14 +0500

    ref

--- Sum of pairs/sumPairs.js
+++ Sum of pairs/sumPairs.js
@@ -2,36 +2,39 @@
 function sumPairs(ints, s) {
 
     'use strict';
-
+  
     let min = ints.length;
-    let result;
+    let obj = {i: 0, j: min, doIt: false};
+    let numberUse = [];
 
     for (let i = 0; i < ints.length; i++) {
 
-        const r = s - ints[i];
-
-        //console.log('i='+i+' r='+r);
-        //console.log(-6-1);
-
-        min = ints.indexOf(r, i);
-        if (min == -1) {
+        let number = ints[i];
+        if (numberUse.includes(number)) {
             continue;
         } else {
-            if (min != i) {
-                result = [ints[i], ints[min]];
-            } else {
-                break;
-            }
+            numberUse.push(number);
         }
 
-        if (i > min) {
+        const r = s - ints[i];
+        min = ints.indexOf(r, i + 1);
+        if (min == -1) {
+            continue;
+        }
+
+        if (min < obj.j) {
+            obj.i = i;
+            obj.j = min;
+            obj.doIt = true;
+        }
+
+        if (obj.i > min) {
             break;
         }
 
     }
 
-    return result;
+    return obj.doIt ? [ints[obj.i], ints[obj.j]] : undefined;
 }
 
-//console.log(sumPairs([1, 4, 8, 7, 3, 15], 8));
 console.log(sumPairs([1, 2, 3, 4, 1, 0], 2));
