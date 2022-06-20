9bd4f07201a83bbce45f65c5e32537c51a80d698
ifkerri <ifkerri@mail.ru>
2022-06-20 12:54:00 +0500

    fundamentals

--- Fundamentals/createPhoneNumber.js
+++ Fundamentals/createPhoneNumber.js
@@ -9,7 +9,10 @@
 
 function createPhoneNumber(numbers){
   
-    
+	const p = numbers.slice(0, 3).join('');
+	const f = numbers.slice(3, 6).join('');
+	const l = numbers.slice(6).join('');
+	return `(${p}) ${f}-${l}`;
 
 }
 
